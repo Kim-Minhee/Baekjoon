bdc354dc002ae14c8fec6659d9aee7bf7c8c3c0e
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2023-05-04 14:25:39 +0900

    [Bronze I] Title: 나무 조각, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2947. 나무 조각/나무 조각.py
@@ -0,0 +1,8 @@
+num = list(map(int, input().split()))
+while True:
+  for i in range(4):
+    if num[i]>num[i+1]:
+      num[i], num[i+1] = num[i+1], num[i]
+      print(*num)
+  if num == [1, 2, 3, 4, 5]:
+    break
