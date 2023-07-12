144e43e72613ea217eaccf883744089bf5bfee82
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2023-07-13 00:05:17 +0900

    [Bronze IV] Title: Rain Diary, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/27182. Rain Diary/Rain Diary.py
@@ -0,0 +1,23 @@
+n, m = map(int, input().split())
+if n-7>0:
+  print(n-7)
+elif n-m==-14:
+  if m+7>28:
+    print(n-7)
+  else:
+    print(m+7)
+elif n-m==-15:
+  if m+7>29:
+    print(n-7)
+  else:
+    print(m+7)
+elif n-m==-16:
+  if m+7>30:
+    print(n-7)
+  else:
+    print(m+7)
+elif n-m==-17:
+  if m+7>31:
+    print(n-7)
+  else:
+    print(m+7)
