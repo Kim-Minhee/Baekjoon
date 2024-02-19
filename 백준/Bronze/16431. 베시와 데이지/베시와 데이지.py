a126d57460414ba40267d9b29b4287442fe878dd
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2024-02-19 23:17:31 +0900

    [Bronze III] Title: 베시와 데이지, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/16431. 베시와 데이지/베시와 데이지.py
@@ -0,0 +1,11 @@
+br, bc = map(int, input().split())
+dr, dc = map(int, input().split())
+jr, jc = map(int, input().split())
+b = max(abs(br-jr), abs(bc-jc))
+d = abs(dr-jr)+abs(dc-jc)
+if b>d:
+  print('daisy')
+elif b<d:
+  print('bessie')
+else:
+  print('tie')
