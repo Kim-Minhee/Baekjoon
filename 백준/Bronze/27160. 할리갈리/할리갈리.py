9c609b77eb2d9061bf9f50a9649457e7444219f1
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-02-03 09:52:37 +0900

    [Bronze II] Title: 할리갈리, Time: 100 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/27160. 할리갈리/할리갈리.py
+++ 백준/Bronze/27160. 할리갈리/할리갈리.py
@@ -1,7 +1,9 @@
+import sys
+
 s = {'STRAWBERRY':0, 'BANANA':0, 'LIME':0, 'PLUM':0}
 
-for _ in range(int(input())):
-  S, X = input().split()
+for _ in range(int(sys.stdin.readline())):
+  S, X = sys.stdin.readline().split()
   s[S] += int(X)
 
 if 5 in s.values():
