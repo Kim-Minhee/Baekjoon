331aafe61b90c1089bb14ea3f0417b4078b29f18
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-02-19 08:05:14 +0900

    [Bronze II] Title: 자작나무가 없소~, Time: 128 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/31496. 자작나무가 없소～/자작나무가 없소～.py
+++ 백준/Bronze/31496. 자작나무가 없소～/자작나무가 없소～.py
@@ -1,8 +1,10 @@
-N, S = input().split()
+import sys
+
+N, S = sys.stdin.readline().split()
 
 r = 0
 for _ in range(int(N)):
-  I, C = input().split()
+  I, C = sys.stdin.readline().split()
   if S in I.split('_'):
     r += int(C)
 
