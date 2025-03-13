b021d3703ec15389eebe7bfa7cb082458eb8c810
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-03-14 00:20:05 +0900

    [Bronze II] Title: Vigenère Cipher Encryption, Time: 1100 ms, Memory: 33192 KB -BaekjoonHub

--- 백준/Bronze/3417. Vigenère Cipher Encryption/Vigenère Cipher Encryption.py
+++ 백준/Bronze/3417. Vigenère Cipher Encryption/Vigenère Cipher Encryption.py
@@ -1,13 +1,11 @@
-a = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-
+# GPT
 while True:
-  K = input()
-  if K=='0': break
+    K = input()
+    if K=='0': break
 
-  c = ''
-  P = input()
-  for i, p in enumerate(P):
-    k = K[i%len(K)]
-    c += a[(a.index(p)+a.index(k)+1)%26]
+    P = input()
+    c = ''.join(
+        chr((ord(p)-65+ord(K[i%len(K)])-65+1)%26+65) for i, p in enumerate(P)
+    )
 
-  print(c)
+    print(c)
