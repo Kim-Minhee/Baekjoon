807d6ef0b32560a66096f3f4067973473adb9f89
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-03-20 23:17:33 +0900

    [Bronze II] Title: Numbersrebmun, Time: 92 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/4141. Numbersrebmun/Numbersrebmun.py
+++ 백준/Bronze/4141. Numbersrebmun/Numbersrebmun.py
@@ -1,20 +1,17 @@
-alpha_num = {'A': 2, 'B': 2, 'C': 2,
-             'D': 3, 'E': 3, 'F': 3,
-             'G': 4, 'H': 4, 'I': 4,
-             'J': 5, 'K': 5, 'L': 5,
-             'M': 6, 'N': 6, 'O': 6,
-             'P': 7, 'Q': 7, 'R': 7, 'S': 7,
-             'T': 8, 'U': 8, 'V': 8,
-             'W': 9, 'X': 9, 'Y': 9, 'Z':9}
+# GPT
+alpha_num = {
+    'A': 2, 'B': 2, 'C': 2,
+    'D': 3, 'E': 3, 'F': 3,
+    'G': 4, 'H': 4, 'I': 4,
+    'J': 5, 'K': 5, 'L': 5,
+    'M': 6, 'N': 6, 'O': 6,
+    'P': 7, 'Q': 7, 'R': 7, 'S': 7,
+    'T': 8, 'U': 8, 'V': 8,
+    'W': 9, 'X': 9, 'Y': 9, 'Z': 9
+}
 
 for _ in range(int(input())):
-  NAME = input().upper()
+    NAME = input().upper()  # 대문자로 변환
+    num = ''.join(str(alpha_num[n]) for n in NAME)
 
-  num = ''
-  for n in NAME:
-    num += str(alpha_num[n])
-
-  if num==num[::-1]:
-    print('YES')
-  else:
-    print('NO')
+    print('YES' if num == num[::-1] else 'NO')
