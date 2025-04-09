8a4a2a1feb6b419082b0759a4f7ca864ce6e7309
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-04-09 20:37:57 +0900

    [Bronze II] Title: Look and Say, Time: 416 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/4841. Look and Say/Look and Say.py
+++ 백준/Bronze/4841. Look and Say/Look and Say.py
@@ -1,17 +1,10 @@
+# GPT
+from itertools import groupby
+
 for _ in range(int(input())):
-  S = input()
-
-  change_num_index = [0]
-  for i in range(1, len(S)):
-    if S[i-1]!=S[i]:
-      change_num_index.append(i)
-
-  r = ''
-  for index in range(len(change_num_index)-1):
-    r += str(change_num_index[index+1]-change_num_index[index])
-    r += str(S[change_num_index[index]])
-
-  r += str(len(S)-change_num_index[-1])
-  r += str(S[change_num_index[-1]])
-
-  print(r)
+    S = input()
+    result = ''
+    for digit, group in groupby(S):
+        count = len(list(group))
+        result += str(count) + digit
+    print(result)
