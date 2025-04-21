3e99f413dc200f7a35d78fc5cd515df163c1c303
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-04-21 20:43:42 +0900

    [Bronze II] Title: Oil Exploration, Time: 84 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5139. Oil Exploration/Oil Exploration.py
+++ 백준/Bronze/5139. Oil Exploration/Oil Exploration.py
@@ -1,28 +1,32 @@
+# GPT
 for k in range(int(input())):
-  h, w = map(int, input().split())
-  layers = ''.join(input() for _ in range(h))
-  layers_trans = ''
-  for i in range(w):
-    layers_trans += layers[i]
-    for j in range(1, h):
-      layers_trans += layers[i+w*j]
+    h, w = map(int, input().split())
+    sea_map = [input() for _ in range(h)]
 
-  costs = []
-  for l in range(0, h*w, h):
-    layer_trans = layers_trans[l:l+h]
-    if 'X' not in layer_trans:
-      costs.append('N')
-    else:
-      cost = 0
-      for layer in layer_trans:
-        if layer=='H':
-          cost += 3
-        elif layer=='S':
-          cost += 1
-        elif layer=='X':
-          break
-      costs.append(str(cost))
+    costs = []
+    for col in range(w):
+        cost = 0
+        found_oil = False
 
-  if k!=0: print()
-  print(f'Data Set {k+1}:')
-  print(*costs)
+        # 위에서부터 아래로 탐색
+        for row in range(h):
+            cell = sea_map[row][col]
+            if cell == 'X':
+                found_oil = True
+                break
+            elif cell == 'H':
+                cost += 3
+            elif cell == 'S':
+                cost += 1
+
+        # 결과 저장
+        if found_oil:
+            costs.append(str(cost))
+        else:
+            costs.append('N')
+
+    # 출력 형식
+    if k != 0:
+        print()
+    print(f'Data Set {k+1}:')
+    print(' '.join(costs))
