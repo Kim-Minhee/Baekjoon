a5c2d418188b474fc37e6bb676b297d33cbcb92b
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-04-24 21:00:41 +0900

    [Bronze II] Title: School Colors, Time: 104 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5205. School Colors/School Colors.py
+++ 백준/Bronze/5205. School Colors/School Colors.py
@@ -1,19 +1,21 @@
+# GPT
 K = int(input())
-for k in range(1, K+1):
-  N = int(input())
-  RGB = []
-  for _ in range(N):
-    RGB.append(tuple(map(int, input().split())))
-  
-  distances = []
-  max_dist = 0
-  for i in range(N-1):
-    for j in range(i+1, N):
-      dist = (RGB[i][0]-RGB[j][0])**2+(RGB[i][1]-RGB[j][1])**2+(RGB[i][2]-RGB[j][2])**2
-      distances.append((i, j, dist))
-      if dist>max_dist: max_dist = dist
+for k in range(1, K + 1):
+    N = int(input())
+    RGB = [tuple(map(int, input().split())) for _ in range(N)]
 
-  print(f'Data Set {k}:')
-  for i, j, d in distances:
-    if d==max_dist:
-      print(i+1, j+1)
+    max_dist = -1
+    results = []
+
+    for i in range(N - 1):
+        for j in range(i + 1, N):
+            dist = (RGB[i][0] - RGB[j][0]) ** 2 + (RGB[i][1] - RGB[j][1]) ** 2 + (RGB[i][2] - RGB[j][2]) ** 2
+            if dist > max_dist:
+                max_dist = dist
+                results = [(i + 1, j + 1)]
+            elif dist == max_dist:
+                results.append((i + 1, j + 1))
+
+    print(f'Data Set {k}:')
+    for i, j in results:
+        print(i, j)
