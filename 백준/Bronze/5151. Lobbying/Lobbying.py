36858e605c1c534f32895906cff4f79c7a694dcc
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-04-28 20:16:50 +0900

    [Bronze II] Title: Lobbying, Time: 2584 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5151. Lobbying/Lobbying.py
+++ 백준/Bronze/5151. Lobbying/Lobbying.py
@@ -1,24 +1,27 @@
+# GPT
 K = int(input())
 for k in range(1, K+1):
     N, M, T1 = map(int, input().split())
 
     d = [0]*N
     for _ in range(M):
-      I, T2, D = map(float, input().split())
-      if 0<=T1-T2<1000:
-        d[int(I)-1] += D
+        I, T2, D = input().split()
+        I = int(I)
+        T2 = int(T2)
+        D = float(D)
+        if 0 <= T1 - T2 < 1000:
+            d[I-1] += D
 
-    v = []
-    for _ in range(N):
-      v.append(input())
+    v = [input() for _ in range(N)]
 
     y, n = 0, 0
     for i, vote in enumerate(v):
-      if vote=='N':
-        n += 1/(1+(d[i]/10000))
-      else:
-        y += 1
+        if vote == 'N':
+            n += 1 / (1 + (d[i] / 10000))
+        else:
+            y += 1
 
-    if k!=1: print()
+    if k != 1:
+        print()
     print(f'Data Set {k}:')
-    print(format(round(y, 2), '.2f'), format(round(n, 2), '.2f'))
+    print(f'{y:.2f} {n:.2f}')
