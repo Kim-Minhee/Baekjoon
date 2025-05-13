4ea7d2a2057d4a1ca6ab0e1b566f6bd9ef64090f
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-05-13 12:50:03 +0900

    [Bronze II] Title: Tautogram, Time: 88 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5698. Tautogram/Tautogram.py
+++ 백준/Bronze/5698. Tautogram/Tautogram.py
@@ -1,16 +1,9 @@
+# GPT
 while True:
-  S = input().lower()
-  if S=='*': break
+    S = input().lower()
+    if S == '*':
+        break
 
-  s_list = S.split()
-  start_s = s_list[0][0]
-  chk = True
-  for s in s_list[1:]:
-    if s[0]!=start_s:
-      chk = False
-      break
-
-  if chk:
-    print('Y')
-  else:
-    print('N')
+    words = S.split()
+    first = words[0][0]
+    print('Y' if all(w[0] == first for w in words) else 'N')
