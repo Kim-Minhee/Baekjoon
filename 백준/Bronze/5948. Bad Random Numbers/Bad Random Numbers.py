5154e1ca17314cb6ff20b8a896975c9bded8c3d6
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-05-20 13:46:52 +0900

    [Bronze II] Title: Bad Random Numbers, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5948. Bad Random Numbers/Bad Random Numbers.py
+++ 백준/Bronze/5948. Bad Random Numbers/Bad Random Numbers.py
@@ -1,21 +1,22 @@
+# Claude
 def random_num(num):
-  len_num = len(str(num))
-  if len_num==1:
-    return 0
-  elif len_num==2:
-    return int(str(num)[0])**2
-  else:
-    return int(str(num)[-3:-1])**2
+    num_str = str(num).zfill(4)
+
+    middle = int(num_str[-3:-1])
+    
+    return middle * middle
 
 N = int(input())
 
 nums = []
 i = 0
 while True:
-  i += 1
-  N = random_num(N)
-  if N not in nums:
     nums.append(N)
-  else:
-    print(i)
-    break
+    
+    N = random_num(N)
+    
+    i += 1
+    
+    if N in nums:
+        print(i)
+        break
