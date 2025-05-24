c9cbac1785e14a43729e7f1cd242691e1ca04dd8
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-05-24 14:56:58 +0900

    [Bronze II] Title: The Robot Plow, Time: 108 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/5989. The Robot Plow/The Robot Plow.py
+++ 백준/Bronze/5989. The Robot Plow/The Robot Plow.py
@@ -1,15 +1,12 @@
+# GPT
 X, Y, I = map(int, input().split())
 
 land = [[0]*X for _ in range(Y)]
 
 for _ in range(I):
-  X_LD, Y_LD, X_RU, Y_RU = map(int, input().split())
-  for y in range(Y_LD-1, Y_RU):
-    for x in range(X_LD-1, X_RU):
-      if not land[y][x]:
-        land[y][x] = 1
+    X_LD, Y_LD, X_RU, Y_RU = map(int, input().split())
+    for y in range(Y_LD - 1, Y_RU):
+        for x in range(X_LD - 1, X_RU):
+            land[y][x] = 1
 
-r = 0
-for i in range(Y):
-  r += sum(land[i])
-print(r)
+print(sum(map(sum, land)))
