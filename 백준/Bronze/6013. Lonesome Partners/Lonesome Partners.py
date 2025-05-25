de5101c653aec29bd38bcb58fe8702f9dcaae4e3
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-05-25 09:21:21 +0900

    [Bronze II] Title: Lonesome Partners, Time: 92 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/6013. Lonesome Partners/Lonesome Partners.py
+++ 백준/Bronze/6013. Lonesome Partners/Lonesome Partners.py
@@ -1,15 +1,19 @@
+# Claude
 N = int(input())
 cows = []
 for _ in range(N):
-  cows.append(tuple(map(int, input().split())))
+    cows.append(tuple(map(int, input().split())))
 
-cow1, cow2 = -1, -1
-max_dist = 0
+# 첫 번째 쌍으로 초기화
+cow1, cow2 = 1, 2
+max_dist_sq = (cows[0][0] - cows[1][0])**2 + (cows[0][1] - cows[1][1])**2
+
+# 모든 쌍을 확인
 for i in range(N-1):
-  for j in range(i+1, N):
-    dist = (cows[i][0]-cows[j][0])**2+(cows[i][1]-cows[j][1])**2
-    if dist>max_dist:
-      cow1, cow2 = i+1, j+1
-      max_dist = dist
+    for j in range(i+1, N):
+        dist_sq = (cows[i][0] - cows[j][0])**2 + (cows[i][1] - cows[j][1])**2
+        if dist_sq > max_dist_sq:
+            cow1, cow2 = i+1, j+1
+            max_dist_sq = dist_sq
 
 print(cow1, cow2)
