93b0c7062f674eb8bebc4702e1610abf43c806b2
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-06-08 14:16:35 +0900

    [Bronze II] Title: From 1987 to 2013, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/6751. From 1987 to 2013/From 1987 to 2013.py
+++ 백준/Bronze/6751. From 1987 to 2013/From 1987 to 2013.py
@@ -1,7 +1,10 @@
+# Gemini 2.5 Pro
 Y = int(input())
 
 while True:
-  Y += 1
-  if len(set(str(Y)))==len(list(str(Y))):
-    print(Y)
-    break
+    Y += 1
+    # 연도를 문자열로 변환한 것의 길이와,
+    # 그것을 set으로 변환한 것의 길이가 같으면 모든 숫자가 고유하다.
+    if len(set(str(Y))) == len(str(Y)):
+        print(Y)
+        break
