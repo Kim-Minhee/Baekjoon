3a2c500f320e4b1ac2b655582a5589379db2fcb4
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-06-14 12:58:42 +0900

    [Bronze II] Title: Old Fishin’ Hole, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/6811. Old Fishin’ Hole/Old Fishin’ Hole.py
+++ 백준/Bronze/6811. Old Fishin’ Hole/Old Fishin’ Hole.py
@@ -1,14 +1,13 @@
+# GPT 4o
 P1, P2, P3, TP = int(input()), int(input()), int(input()), int(input())
-if TP < P1 and TP < P2 and TP < P3:
-  print('Number of ways to catch fish: 0')
-else:
-  max_p1, max_p2, max_p3 = TP // P1, TP // P2, TP // P3
-  case_cnt = 0
-  for p1 in range(max_p1 + 1):
-    for p2 in range(max_p2 + 1):
-      for p3 in range(max_p3 + 1):
-        total_points = p1 * P1 + p2 * P2 + p3 * P3
-        if p1 + p2 + p3 != 0 and total_points<=TP:
-          case_cnt += 1
-          print(f'{p1} Brown Trout, {p2} Northern Pike, {p3} Yellow Pickerel')
-  print(f'Number of ways to catch fish: {case_cnt}')
+
+max_p1, max_p2, max_p3 = TP // P1, TP // P2, TP // P3
+case_cnt = 0
+for p1 in range(max_p1 + 1):
+  for p2 in range(max_p2 + 1):
+    for p3 in range(max_p3 + 1):
+      total_points = p1 * P1 + p2 * P2 + p3 * P3
+      if p1 + p2 + p3 != 0 and total_points<=TP:
+        case_cnt += 1
+        print(f'{p1} Brown Trout, {p2} Northern Pike, {p3} Yellow Pickerel')
+print(f'Number of ways to catch fish: {case_cnt}')
