88a06a42f69ddb170f0fb28a10a20dd28f7b6cc2
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-06-17 13:10:57 +0900

    [Bronze II] Title: Maternity, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/6860. Maternity/Maternity.py
+++ 백준/Bronze/6860. Maternity/Maternity.py
@@ -1,17 +1,34 @@
-MG, FG = list(input()), list(input())
+# GPT 4o
+# 입력 처리
+mother = input()
+father = input()
 
-dominant, recessive = 'ABCDE', 'abcde'
+dominant = 'ABCDE'
+recessive = 'abcde'
 possible_genes = []
-for i in range(0, 10, 2):
-  g1 = recessive[i//2] if ''.join(MG[i] + FG[i]).islower() else dominant[i//2]
-  g2 = recessive[i//2] if ''.join(MG[i] + FG[i+1]).islower() else dominant[i//2]
-  g3 = recessive[i//2] if ''.join(MG[i+1] + FG[i]).islower() else dominant[i//2]
-  g4 = recessive[i//2] if ''.join(MG[i+1] + FG[i+1]).islower() else dominant[i//2]
-  possible_genes.append(set([g1, g2, g3, g4]))
 
+# 각 유전자 자리마다 가능한 표현형을 계산
+for i in range(0, 10, 2):
+    m_alleles = [mother[i], mother[i+1]]
+    f_alleles = [father[i], father[i+1]]
+    gene_idx = i // 2
+    expressions = set()
+
+    # 부모의 각각의 알렐 조합 생성
+    for m in m_alleles:
+        for f in f_alleles:
+            if m.isupper() or f.isupper():
+                expressions.add(dominant[gene_idx])  # dominant 표현
+            else:
+                expressions.add(recessive[gene_idx])  # recessive 표현
+    possible_genes.append(expressions)
+
+# 아기 유전자 검사
 for _ in range(int(input())):
-  possible = True
-  for idx, bg in enumerate(list(input())):
-    if bg not in possible_genes[idx]:
-      possible = False
-  print('Possible baby.' if possible else 'Not their baby!')
+    baby = input()
+    for i in range(5):
+        if baby[i] not in possible_genes[i]:
+            print("Not their baby!")
+            break
+    else:
+        print("Possible baby.")
