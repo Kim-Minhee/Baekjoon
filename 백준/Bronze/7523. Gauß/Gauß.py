e7230faaed12f8918016c01362f76daa3d724268
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-07-11 20:40:30 +0900

    [Bronze II] Title: Gauß, Time: 68 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/7523. Gauß/Gauß.py
+++ 백준/Bronze/7523. Gauß/Gauß.py
@@ -1,13 +1,10 @@
+# GPT 4o
 T = int(input())
-for i in range(1, T+1):
-  N, M = map(int, input().split())
 
-  cnt = M-N+1
-  r = (M+N)*(cnt//2)
-  if cnt%2!=0:
-    r += N+cnt//2
+for i in range(1, T + 1):
+    n, m = map(int, input().split())
+    total = (m - n + 1) * (n + m) // 2
 
-  if i>1:
+    print(f"Scenario #{i}:")
+    print(total)
     print()
-  print(f'Scenario #{i}:')
-  print(r)
