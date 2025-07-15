7603ae5aa4b2bb1b4a2658c774af967df78e606f
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-07-15 20:58:11 +0900

    [Bronze II] Title: Tennis, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/7597. Tennis/Tennis.py
+++ 백준/Bronze/7597. Tennis/Tennis.py
@@ -1,20 +1,31 @@
+# GPT 4o
+def calculate_games(points):
+    score_a = 0
+    score_b = 0
+    games_a = 0
+    games_b = 0
+
+    for p in points:
+        if p == 'A':
+            score_a += 1
+        elif p == 'B':
+            score_b += 1
+
+        # 게임이 끝나는 조건: 4점 이상 + 상대보다 2점 이상 앞섬
+        if score_a >= 4 and score_a - score_b >= 2:
+            games_a += 1
+            score_a = 0
+            score_b = 0
+        elif score_b >= 4 and score_b - score_a >= 2:
+            games_b += 1
+            score_a = 0
+            score_b = 0
+
+    return games_a, games_b
+
 while True:
-  LINE = input()
-  if LINE=='#': break
-
-  x, y = 0, 0
-  score_a, score_b = 0, 0
-  for winner in LINE:
-    if winner=='A':
-      score_a += 1
-    else:
-      score_b += 1
-
-    if score_a>=4 and score_a-score_b>=2:
-      x += 1
-      score_a, score_b = 0, 0
-    elif score_b>=4 and score_b-score_a>=2:
-      y += 1
-      score_a, score_b = 0, 0
-
-  print(f'A {x} B {y}')
+    line = input().strip()
+    if line == '#':
+        break
+    a_games, b_games = calculate_games(line)
+    print(f"A {a_games} B {b_games}")
