a54301583aea54849c6495ea345c07edfb63f1b7
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-07-17 00:51:47 +0900

    [Bronze II] Title: Bookings, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/7598. Bookings/Bookings.py
+++ 백준/Bronze/7598. Bookings/Bookings.py
@@ -1,16 +1,25 @@
+# GPT 4o
 while True:
-  PLAIN, N = input().split()
-  if PLAIN=='#' and N=='0': break
-  
-  booking = int(N)
-  while True:
-    TYPE, S = input().split()
-    if TYPE=='X' and S=='0': break
+    line = input()
+    if line == "# 0":
+        break
 
-    s = int(S)
-    if TYPE=='B' and s<=68-booking:
-      booking += s
-    elif TYPE=='C' and s<=booking:
-      booking -= s
-    
-  print(PLAIN, booking)
+    flight, booked = line.split()
+    booked = int(booked)
+
+    while True:
+        tx = input()
+        if tx == "X 0":
+            break
+
+        action, number = tx.split()
+        number = int(number)
+
+        if action == "B":
+            if booked + number <= 68:
+                booked += number
+        elif action == "C":
+            if number <= booked:
+                booked -= number
+
+    print(f"{flight} {booked}")
