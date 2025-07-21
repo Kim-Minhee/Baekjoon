aa1f6281c2d78c54cb12a73a3230abbd8c6c2c10
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-07-22 00:58:00 +0900

    [Bronze II] Title: Joke, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/7790. Joke/Joke.py
+++ 백준/Bronze/7790. Joke/Joke.py
@@ -1,11 +1,5 @@
-# GPT 4o
 import sys
 
-# 전체 입력 텍스트 읽기 (파일 크기 최대 20KB)
-text = sys.stdin.read()
-
-# 'joke'라는 부분 문자열 개수 세기
-count = text.count('joke')
-
-# 결과 출력
-print(count)
+S = sys.stdin.read()
+r = S.count('joke')
+print(r)
