bf8542acf5d86ef7295e45e93f7d0904276ffb6c
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-07-25 02:22:15 +0900

    [Bronze II] Title: Potęga, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/8559. Potęga/Potęga.py
+++ 백준/Bronze/8559. Potęga/Potęga.py
@@ -1,7 +1,10 @@
-N = int(input())
+# GPT 4o
+n = input().lstrip('0')  # 0으로 시작하는 경우 제거
 
-r_list = [2, 4, 8, 6]
-if N==0:
-  print(1)
+# n이 0이면 2⁰ = 1
+if n == '':
+    print(1)
 else:
-  print(r_list[N%4-1])
+    last_digit_cycle = [6, 2, 4, 8]  # index: n % 4
+    n_mod_4 = int(n[-2:]) % 4  # n의 마지막 두 자리만 보고 % 4 계산
+    print(last_digit_cycle[n_mod_4])
