3bb330dc1d2e4bc8b29eb542d9fa8cc4c03668ea
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-07-31 04:00:41 +0900

    [Bronze II] Title: Wężyk, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/8712. Wężyk/Wężyk.py
+++ 백준/Bronze/8712. Wężyk/Wężyk.py
@@ -1,12 +1,10 @@
 # GPT 4o
-n = int(input())
+N = int(input())
 
-for i in range(n):
-    row_start = i * n + 1
-    row_end = (i + 1) * n + 1
-    row = list(range(row_start, row_end))
-    
-    if i % 2 == 1:  # 짝수 번째 줄 (인덱스 기준, 실제로는 홀수 줄): 오른쪽 → 왼쪽
+reverse = False
+for n in range(1, N**2 + 1, N):
+    row = list(range(n, n + N))
+    if reverse:
         row.reverse()
-    
     print(' '.join(map(str, row)))
+    reverse = not reverse
