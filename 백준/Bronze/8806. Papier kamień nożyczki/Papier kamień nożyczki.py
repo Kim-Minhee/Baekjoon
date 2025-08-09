ca38d816f7ed68124a573cebf89469ec1c9103f3
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-08-09 19:53:44 +0900

    [Bronze II] Title: Papier kamień nożyczki, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/8806. Papier kamień nożyczki/Papier kamień nożyczki.py
+++ 백준/Bronze/8806. Papier kamień nożyczki/Papier kamień nożyczki.py
@@ -1,12 +1,17 @@
-for _ in range(int(input())):
-    X1, Y1, Z1 = map(float, input().split())
-    X2, Y2, Z2 = map(float, input().split())
+# GPT 4o
+T = int(input().strip())
+for _ in range(T):
+    X1, Y1, Z1 = map(float, input().split())  # Adam
+    X2, Y2, Z2 = map(float, input().split())  # Gosia
 
-    adam_win = X1 * Y2 + Y1 * Z2 + Z1 * X2
-    gosia_win = X2 * Y1 + Y2 * Z1 + Z2 * X1
-    if adam_win>gosia_win:
-        print('ADAM')
-    elif gosia_win>adam_win:
-        print('GOSIA')
+    # Adam 승리 확률
+    P_A = X1 * Y2 + Y1 * Z2 + Z1 * X2
+    # Gosia 승리 확률
+    P_G = X2 * Y1 + Y2 * Z1 + Z2 * X1
+
+    if P_A > P_G:
+        print("ADAM")
+    elif P_G > P_A:
+        print("GOSIA")
     else:
-        print('=')
+        print("=")
