9abba23ca09d24f2faed89ca6a6b653c6d5e6fb7
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-08-12 20:04:58 +0900

    [Bronze II] Title: Wymiana żarówki, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/8846. Wymiana żarówki/Wymiana żarówki.py
+++ 백준/Bronze/8846. Wymiana żarówki/Wymiana żarówki.py
@@ -1,5 +1,26 @@
+# GPT 4o
+MOD = 500000009
+
+def mod_exp(base, exp, mod):
+    result = 1
+    cur = base % mod
+    e = exp
+    while e > 0:
+        if e & 1:
+            result = (result * cur) % mod
+        cur = (cur * cur) % mod
+        e >>= 1
+    return result
+
+def mod_inv(a, mod):
+    # 페르마의 소정리: mod가 소수일 때, a^(mod-2) mod mod가 역원
+    return mod_exp(a, mod-2, mod)
+
 A = int(input())
-r = 1
-if A>1:
-    r += 4 * (4 ** (A - 1) - 1) // 3
-print(r % 500000009)
+
+pow_4_A = mod_exp(4, A, MOD)
+inv_3 = mod_inv(3, MOD)
+
+answer = (pow_4_A - 1) * inv_3 % MOD
+
+print(answer)
