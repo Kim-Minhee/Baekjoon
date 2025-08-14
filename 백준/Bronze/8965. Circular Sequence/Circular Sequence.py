7979a16ed5c210e2f41f2502355553968e433dc4
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-08-14 20:59:01 +0900

    [Bronze II] Title: Circular Sequence, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/8965. Circular Sequence/Circular Sequence.py
+++ 백준/Bronze/8965. Circular Sequence/Circular Sequence.py
@@ -1,6 +1,7 @@
-for _ in range(int(input())):
-    S = input()
-    s = S
-    for i in range(1, len(S)):
-        s = min(s, S[i:]+S[:i])
-    print(s)
+# GPT 4o
+T = int(input())
+for _ in range(T):
+    s = input().strip()
+    # 모든 회전 중 사전순 최소 선택
+    min_rotation = min(s[i:] + s[:i] for i in range(len(s)))
+    print(min_rotation)
