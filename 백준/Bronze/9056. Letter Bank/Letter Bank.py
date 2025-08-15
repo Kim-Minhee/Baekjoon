89eaaa4cd727f580cf767efb0ad9f34d88929d05
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-08-15 17:36:50 +0900

    [Bronze II] Title: Letter Bank, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9056. Letter Bank/Letter Bank.py
+++ 백준/Bronze/9056. Letter Bank/Letter Bank.py
@@ -1,6 +1,12 @@
-for _ in range(int(input())):
-    A, B = map(set, input().split())
-    if A==B:
-        print('YES')
+# GPT 5
+T = int(input().strip())
+for _ in range(T):
+    alpha, beta = input().strip().split()
+    
+    set_alpha = set(alpha)
+    set_beta = set(beta)
+    
+    if set_beta.issubset(set_alpha) and set_alpha.issubset(set_beta):
+        print("YES")
     else:
-        print('NO')
+        print("NO")
