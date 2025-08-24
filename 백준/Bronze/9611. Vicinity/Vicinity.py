c528489ed2da023bdac92f2775b8fee18dcc8fff
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-08-24 20:45:40 +0900

    [Bronze II] Title: Vicinity, Time: 72 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9611. Vicinity/Vicinity.py
+++ 백준/Bronze/9611. Vicinity/Vicinity.py
@@ -1,17 +1,21 @@
-N = int(input())
-p = [[]]
-for _ in range(N):
-    p.append(list(map(int, input().split())))
+# GPT 5
+def main():
+    n = int(input())
+    points = [tuple(map(int, input().split())) for _ in range(n)]
 
-for _ in range(int(input())):
-    I, DV = map(int, input().split())
-    xi, yi = p[I][0], p[I][1]
+    t = int(input())
+    for _ in range(t):
+        i, dv = map(int, input().split())
+        xi, yi = points[i - 1]  # 입력은 1-based라서 보정
+        dv2 = dv * dv
 
-    cnt = 0
-    for i in range(1, N+1):
-        if i != I:
-            x, y = p[i][0], p[i][1]
-            d = ((xi - x) ** 2 + (yi - y) ** 2) ** 0.5
-            if d <= DV:
-                cnt += 1
-    print(cnt)
+        count = 0
+        for j, (x, y) in enumerate(points):
+            if j == i - 1:  # 자기 자신 제외
+                continue
+            if (xi - x) ** 2 + (yi - y) ** 2 <= dv2:
+                count += 1
+        print(count)
+
+if __name__ == "__main__":
+    main()
