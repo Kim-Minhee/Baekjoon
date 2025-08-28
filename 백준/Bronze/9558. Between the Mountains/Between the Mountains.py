311faa86ab7821a6c72ee6300d4209da5115a27e
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-08-28 21:53:49 +0900

    [Bronze II] Title: Between the Mountains, Time: 4296 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9558. Between the Mountains/Between the Mountains.py
+++ 백준/Bronze/9558. Between the Mountains/Between the Mountains.py
@@ -1,13 +1,17 @@
-import sys
-input = sys.stdin.readline
-
 for _ in range(int(input())):
     A = list(map(int, input().split()))
     B = list(map(int, input().split()))
 
+    A = set(A[1:])
+    A = list(A)
+    A.sort()
+    B = set(B[1:])
+    B = list(B)
+    B.sort()
+    
     min_diff = 1000000
-    for a in set(A[1:]):
-        for b in set(B[1:]):
+    for a in A:
+        for b in B:
             diff = abs(a - b)
             min_diff = min(min_diff, diff)
         if min_diff == 0:
