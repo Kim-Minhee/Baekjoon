ac792378d1e3eae284b63514a205c70aabb0c28d
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-09-01 21:31:01 +0900

    [Bronze II] Title: Max, Time: 36 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Bronze/9913. Max/Max.py
+++ 백준/Bronze/9913. Max/Max.py
@@ -1,8 +1,17 @@
-from collections import Counter
+# GPT 5
+import sys
 
-nums = []
-for _ in range(int(input())):
-    nums.append(int(input()))
+def main():
+    data = list(map(int, sys.stdin.buffer.read().split()))
+    n = data[0]
+    nums = data[1:1+n]
 
-counter = Counter(nums)
-print(max(counter.values()))
+    # 값 범위가 1..1000이므로 고정 크기 배열로 빠르게 카운트
+    counts = [0] * 1001
+    for x in nums:
+        counts[x] += 1
+
+    print(max(counts))
+
+if __name__ == "__main__":
+    main()
