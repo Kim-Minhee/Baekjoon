def68b99e5ec07001371081c1c5e11f95448dc3f
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-09-05 23:10:52 +0900

    [Bronze II] Title: 아즈텍 피라미드, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/7770. 아즈텍 피라미드/아즈텍 피라미드.py
+++ 백준/Bronze/7770. 아즈텍 피라미드/아즈텍 피라미드.py
@@ -1,13 +1,23 @@
-N = int(input())
+# GPT 5
+import sys
 
-N -= 1
-floor = 1
-while True:
-    block = [i for i in range(1, 2 * (floor + 1), 2)]
-    min_block = sum(block) + sum(block[:-1])
-    N -= min_block
-    if N < 0:
-        break
-    floor += 1
+def max_height(n: int) -> int:
+    h = 0
+    used = 0
+    # 다음 층(h+1)을 올리기 위해 필요한 블록 수: 2h^2 + 2h + 1
+    while True:
+        need = 2*h*h + 2*h + 1
+        if used + need > n:
+            return h
+        used += need
+        h += 1
 
-print(floor)
+def main():
+    data = sys.stdin.read().split()
+    if not data:
+        return
+    n = int(data[0])
+    print(max_height(n))
+
+if __name__ == "__main__":
+    main()
