8b48fee1255df0bbb81afbcbc45d1b8cf5cf8d79
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-09-07 19:18:02 +0900

    [Bronze II] Title: Have you had your birthday yet?, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9948. Have you had your birthday yet？/Have you had your birthday yet？.py
+++ 백준/Bronze/9948. Have you had your birthday yet？/Have you had your birthday yet？.py
@@ -1,17 +1,30 @@
-import calendar
+# GPT 5
+import sys
 
-month = list(calendar.month_name)
-while True:
-    B = input()
-    if B == '0 #':
+# 월 이름을 숫자로 매핑
+months = {
+    "January": 1, "February": 2, "March": 3, "April": 4,
+    "May": 5, "June": 6, "July": 7, "August": 8,
+    "September": 9, "October": 10, "November": 11, "December": 12
+}
+
+for line in sys.stdin:
+    line = line.strip()
+    if line == "0 #":
         break
-    
-    d, m = B.split()
-    if m == 'February' and d == '29':
-        print('Unlucky')
-    elif m == 'August' and d == '4':
-        print('Happy birthday')
-    elif month.index(m) < 8 or (month.index(m) == 8 and int(d) < 4):
-        print('Yes')
+
+    day, month = line.split()
+    day = int(day)
+    month_num = months[month]
+
+    # 특별 조건 먼저 확인
+    if day == 4 and month == "August":
+        print("Happy birthday")
+    elif day == 29 and month == "February":
+        print("Unlucky")
     else:
-        print('No')
+        # 기준 날짜: 2007년 8월 4일
+        if (month_num < 8) or (month_num == 8 and day < 4):
+            print("Yes")
+        else:
+            print("No")
