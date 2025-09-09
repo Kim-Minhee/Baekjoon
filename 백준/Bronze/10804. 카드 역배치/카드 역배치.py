9d8e95954e0ac7543051ef95104c34c4b9db18aa
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-09-09 17:57:28 +0900

    [Bronze II] Title: 카드 역배치, Time: 40 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/10804. 카드 역배치/카드 역배치.py
+++ 백준/Bronze/10804. 카드 역배치/카드 역배치.py
@@ -1,8 +1,11 @@
-cards = [i for i in range(21)]
+# GPT 5
+# 카드 뒤집기 문제 풀이
+
+cards = list(range(1, 21))  # 처음 카드 상태 [1, 2, ..., 20]
 
 for _ in range(10):
-    A, B = map(int, input().split())
-    reversed = cards[A:B + 1][::-1]
-    cards[A:B + 1] = reversed
+    a, b = map(int, input().split())
+    # 파이썬 슬라이싱은 끝 인덱스를 포함하지 않으므로 b 그대로 사용
+    cards[a-1:b] = reversed(cards[a-1:b])
 
-print(' '.join(str(i) for i in cards[1:]))
+print(*cards)
