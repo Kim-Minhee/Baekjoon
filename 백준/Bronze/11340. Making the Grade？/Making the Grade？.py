86d23b5250451584210501f8cfb9b5d101597ad2
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-09-15 23:12:00 +0900

    [Bronze II] Title: Making the Grade?, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Bronze/11340. Making the Grade？/Making the Grade？.py
+++ 백준/Bronze/11340. Making the Grade？/Making the Grade？.py
@@ -1,19 +1,13 @@
-# GPT 5
-import sys
-import math
-
+import sys, math
 input = sys.stdin.readline
-T = int(input().strip())
-for _ in range(T):
-    p, paper, mid = map(int, input().split())
-    base = 15*p + 20*paper + 25*mid  # 100분율로 환산한 부분합
-    need = 9000 - base
-    if need <= 0:
+
+for _ in range(int(input())):
+    P, T, M = map(int, input().split())
+    score = P * 15 + T * 20 + M * 25
+    needed = math.ceil((9000 - score) / 40)
+    if 0 <= needed <= 100:
+        print(needed)
+    elif needed < 0:
         print(0)
     else:
-        # 올림(ceil) 정수로 계산
-        final_needed = (need + 39) // 40  # (need + 40 - 1) // 40
-        if final_needed > 100:
-            print("impossible")
-        else:
-            print(final_needed)
+        print('impossible')
