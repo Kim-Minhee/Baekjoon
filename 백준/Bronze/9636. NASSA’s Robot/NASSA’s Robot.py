d3ffe4f5a03fb50a93700fccf1eb6ed60826f97f
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-09-19 21:58:51 +0900

    [Bronze II] Title: NASSA’s Robot, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9636. NASSA’s Robot/NASSA’s Robot.py
+++ 백준/Bronze/9636. NASSA’s Robot/NASSA’s Robot.py
@@ -1,28 +1,22 @@
+# GPT 5
 import sys
 input = sys.stdin.readline
 
-for _ in range(int(input())):
-    D = input()
+T = int(input().strip())
+for _ in range(T):
+    s = input().strip()
+    u = s.count('U')
+    d = s.count('D')
+    l = s.count('L')
+    r = s.count('R')
+    q = s.count('?')
 
-    current_x, current_y = 0, 0
-    for d in D:
-        if d == '?':
-            continue
-        elif d == 'U':
-            current_y += 1
-        elif d == 'R':
-            current_x += 1
-        elif d == 'D':
-            current_y -= 1
-        elif d == 'L':
-            current_x -= 1
-    
-    if '?' not in D:
-        print(current_x, current_y, current_x, current_y)
-    else:
-        cnt = D.count('?')
-        min_x = current_x - cnt
-        min_y = current_y - cnt
-        max_x = current_x + cnt
-        max_y = current_y + cnt
-        print(min_x, min_y, max_x, max_y)
+    x = r - l
+    y = u - d
+
+    min_x = x - q
+    max_x = x + q
+    min_y = y - q
+    max_y = y + q
+
+    print(min_x, min_y, max_x, max_y)
