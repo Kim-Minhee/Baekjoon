87b57f109347d2c1bcc366e094e473267affd57a
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-09-22 20:08:19 +0900

    [Bronze II] Title: Undercut, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/9865. Undercut/Undercut.py
+++ 백준/Bronze/9865. Undercut/Undercut.py
@@ -1,14 +1,6 @@
 import sys
 input = sys.stdin.readline
 
-def undercut(a, b):
-    if a == 2 and b == 1:
-        return 6
-    elif a - b == 1:
-        return a + b
-    else:
-        return a
-
 for t in range(1, int(input()) + 1):
     M = int(input())
     score_tessa, score_danny = 0, 0
