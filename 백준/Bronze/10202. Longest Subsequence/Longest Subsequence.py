10f1467d2ca66e8ecb6ca439f0961367a94d6a77
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-10-12 20:49:44 +0900

    [Bronze II] Title: Longest Subsequence, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10202. Longest Subsequence/Longest Subsequence.py
+++ 백준/Bronze/10202. Longest Subsequence/Longest Subsequence.py
@@ -1,11 +1,9 @@
+# GPT 5
 import sys
 input = sys.stdin.readline
 
 for _ in range(int(input())):
-    DATA = input().split()
-    ox = ''.join(DATA[1:])
-    max_x = 0
-    for x in ox.split('O'):
-        if x != '' and max_x < len(x):
-            max_x = len(x)
+    data = input().split()
+    ox = ''.join(data[1:])
+    max_x = max((len(x) for x in ox.split('O')), default=0)
     print(f"The longest contiguous subsequence of X's is of length {max_x}")
