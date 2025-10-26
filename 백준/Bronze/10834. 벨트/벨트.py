43b7030fcacb3c8ae0ad1c456162a9e2322815be
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-10-26 18:01:15 +0900

    [Bronze II] Title: 벨트, Time: 60 ms, Memory: 34536 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10834. 벨트/벨트.py
+++ 백준/Bronze/10834. 벨트/벨트.py
@@ -1,10 +1,22 @@
-import sys
-input = sys.stdin.readline
+from math import gcd
 
-d, r = False, 1
-for _ in range(int(input().strip())):
-    A, B, T = map(int, input().split())
-    r *= B / A
-    if T:
-        d = not d
-print(int(d), int(r))
+M = int(input())
+direction = 0  # 0: 시계, 1: 반시계
+num, den = 1, 1  # 현재 회전 비율 (num/den)
+
+for _ in range(M):
+    a, b, s = map(int, input().split())
+    # 비율 업데이트
+    num *= b
+    den *= a
+    # 방향 업데이트 (XOR)
+    direction ^= s
+
+# 약분
+g = gcd(num, den)
+num //= g
+den //= g
+
+# 첫 번째 바퀴가 1회전이므로, 마지막 바퀴는 num/den 회전
+# 항상 정수라 가정됨
+print(direction, num // den)
