1a7c88b72808ab79decec302ecb0bf362f3a59b7
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-10-27 21:10:01 +0900

    [Bronze II] Title: Extreme Sort, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/10855. Extreme Sort/Extreme Sort.py
+++ 백준/Bronze/10855. Extreme Sort/Extreme Sort.py
@@ -1,12 +1,11 @@
+# GPT 5
 import sys
 input = sys.stdin.readline
 
 N = int(input().strip())
-A = list(map(int, input().split()))
+a = list(map(int, input().split()))
 
-chk = True
-for i in range(N - 1):
-    if A[i + 1] - A[i] < 0:
-        chk = False
-        break
-print('yes' if chk else 'no')
+# 비내림 순서인지 확인
+ok = all(a[i] <= a[i+1] for i in range(N-1))
+
+print("yes" if ok else "no")
