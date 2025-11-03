bc011e6451f67186381978210a96ca30e6d00988
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-11-04 00:19:03 +0900

    [Bronze II] Title: A Serious Reading Problem, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11368. A Serious Reading Problem/A Serious Reading Problem.py
+++ 백준/Bronze/11368. A Serious Reading Problem/A Serious Reading Problem.py
@@ -1,9 +1,9 @@
+# GPT 5
 import sys
 input = sys.stdin.readline
 
 while True:
-    DATA = input().strip()
-    if DATA == '0 0 0 0':
+    C, W, L, P = map(int, input().split())
+    if C == W == L == P == 0:
         break
-    C, W, L, P = map(int, DATA.split())
     print(C ** (W * L * P))
