f171545f8ab620ab56e7051b2deb10c66a080038
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-11-07 14:11:25 +0900

    [Bronze II] Title: ACM 대회 채점, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11531. ACM 대회 채점/ACM 대회 채점.py
+++ 백준/Bronze/11531. ACM 대회 채점/ACM 대회 채점.py
@@ -1,23 +1,35 @@
+# GPT 5
 import sys
 input = sys.stdin.readline
 
-right, wrong = {}, {}
-total_pnt = 0
+problems = {}
 
 while True:
-    L = input().strip()
-    if L == '-1':
+    line = input().strip()
+    if line == "-1":
         break
-    m, p, r = L.split()
-    if r == 'right':
-        right[p] = int(m)
-    elif p not in right.keys():
-        wrong[p] = wrong.get(p, 0) + 1
+    time_str, problem, result = line.split()
+    time = int(time_str)
 
-for problem, minutes in right.items():
-    pnt = minutes
-    if problem in wrong.keys():
-        pnt += wrong[problem] * 20
-    total_pnt += pnt
+    if problem not in problems:
+        problems[problem] = {"solved": False, "wrong": 0, "time": 0}
 
-print(len(right), total_pnt)
+    if problems[problem]["solved"]:
+        continue  # 이미 해결된 문제는 무시
+
+    if result == "right":
+        problems[problem]["solved"] = True
+        problems[problem]["time"] = time
+    else:
+        problems[problem]["wrong"] += 1
+
+# 결과 계산
+solved_count = 0
+penalty = 0
+
+for info in problems.values():
+    if info["solved"]:
+        solved_count += 1
+        penalty += info["time"] + info["wrong"] * 20
+
+print(solved_count, penalty)
