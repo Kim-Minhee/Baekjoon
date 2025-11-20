3655d8f0f49d5e4ec4e318a29d46846c18eec45d
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-11-20 19:24:36 +0900

    [Bronze II] Title: NN, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11944. NN/NN.py
+++ 백준/Bronze/11944. NN/NN.py
@@ -1,10 +1,8 @@
+# GPT 5
 import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-l = len(str(N))
-if l * N <= M:
-    print(str(N) * N)
-else:
-    n = str(N) * (M // l + 1)
-    print(n[:M])
+
+result = str(N) * N   # N을 N번 반복한 문자열
+print(result[:M])     # 앞에서 M자리만 출력
