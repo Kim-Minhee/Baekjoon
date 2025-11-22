323ed22a344349e279afa9df7c3806fe53b71c6e
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-11-22 21:21:50 +0900

    [Bronze II] Title: Robot Rock Band (Small), Time: 64 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Bronze/12044. Robot Rock Band （Small）/Robot Rock Band （Small）.py
+++ 백준/Bronze/12044. Robot Rock Band （Small）/Robot Rock Band （Small）.py
@@ -1,14 +1,31 @@
+# GPT 5
 import sys
 input = sys.stdin.readline
 
-for t in range(1, int(input().strip()) + 1):
+T = int(input().strip())
+
+for tc in range(1, T + 1):
     N, K = map(int, input().split())
-    robots = [tuple(map(int, input().split())) for _ in range(4)]
-    cnt = 0
-    for a in robots[0]:
-        for b in robots[1]:
-            for c in robots[2]:
-                for d in robots[3]:
-                    if a ^ b ^ c ^ d == K:
-                        cnt += 1
-    print(f'Case #{t}: {cnt}')
+    
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+    C = list(map(int, input().split()))
+    D = list(map(int, input().split()))
+
+    from collections import Counter
+    
+    # (a ^ b) 값들의 등장 횟수 계산
+    ab_counts = Counter()
+    for a in A:
+        for b in B:
+            ab_counts[a ^ b] += 1
+    
+    answer = 0
+    # (c ^ d)를 구하며 필요한 (a^b) 존재 여부 확인
+    for c in C:
+        for d in D:
+            need = (c ^ d) ^ K
+            if need in ab_counts:
+                answer += ab_counts[need]
+
+    print(f"Case #{tc}: {answer}")
