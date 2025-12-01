6c8c3e70ed3f4aea489dbe77ef2b12da4f4b2d57
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-12-01 22:31:46 +0900

    [Bronze II] Title: Standing Ovation (Small), Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/12166. Standing Ovation （Small）/Standing Ovation （Small）.py
+++ 백준/Bronze/12166. Standing Ovation （Small）/Standing Ovation （Small）.py
@@ -1,24 +1,12 @@
-# GPT 5.1
 import sys
 input = sys.stdin.readline
 
-T = int(input().strip())
-
-for t in range(1, T + 1):
-    Smax, s = input().split()
-    Smax = int(Smax)
-    
+for t in range(1, int(input().strip()) + 1):
+    Smax, Shyness = input().split()
     standing = 0
-    friends = 0
-    
-    for k in range(Smax + 1):
-        cnt = int(s[k])
-        
-        if standing < k:
-            need = k - standing
-            friends += need
-            standing += need
-        
-        standing += cnt
-    
-    print(f"Case #{t}: {friends}")
+    need = 0
+    for shyness, people in enumerate(Shyness):
+        if shyness > standing + need:
+            need += shyness - (standing + need)
+        standing += int(people)
+    print(f'Case #{t}: {need}')
