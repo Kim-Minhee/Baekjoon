de239c7c6e5215164820e123f9d954e5a9fa22ec
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-12-18 19:06:34 +0900

    [Bronze II] Title: huaauhahhuahau, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/13587. huaauhahhuahau/huaauhahhuahau.py
+++ 백준/Bronze/13587. huaauhahhuahau/huaauhahhuahau.py
@@ -1,12 +1,13 @@
+# GPT 5.2
 import sys
 input = sys.stdin.readline
 
-L = input()
-vowel = ''
-for l in L:
-    if l in 'aeiou':
-        vowel += l
-if vowel == vowel[::-1]:
+s = input().strip()
+
+vowels = 'aeiou'
+only_vowels = ''.join(c for c in s if c in vowels)
+
+if only_vowels == only_vowels[::-1]:
     print('S')
 else:
     print('N')
