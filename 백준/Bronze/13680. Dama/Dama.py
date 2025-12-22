fff495c318f23d9140bf4b877d18caa8733056c7
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-12-22 22:52:09 +0900

    [Bronze II] Title: Dama, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/13680. Dama/Dama.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+while True:
+    DATA = input().strip()
+    if DATA == '0 0 0 0':
+        break
+    x1, y1, x2, y2 = map(int, DATA.split())
+    if x1 == x2 and y1 == y2:
+        print(0)
+    elif x1 == x2 or y1 == y2 or abs(x2 - x1) == abs(y2 - y1):
+        print(1)
+    else:
+        print(2)
