84ffa9754d0fe04a4aedbac700e9ca950ba75b65
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2025-12-31 23:21:23 +0900

    [Bronze II] Title: Ragaman, Time: 56 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Bronze/14043. Ragaman/Ragaman.py
+++ 백준/Bronze/14043. Ragaman/Ragaman.py
@@ -1,24 +1,28 @@
-from collections import Counter
+# GPT 5.2
 import sys
+from collections import Counter
+
 input = sys.stdin.readline
 
 A = input().strip()
 B = input().strip()
-a_counter = Counter(A)
-b_counter = Counter(B)
-ana_chk = True
-for key, value in b_counter.items():
-    if key == '*':
+
+cntA = Counter(A)
+cntB = Counter(B)
+
+stars = cntB.get('*', 0)
+
+# B에서 '*'가 아닌 문자들을 A에서 "소비"할 수 있어야 한다.
+for ch, need in cntB.items():
+    if ch == '*':
         continue
-    if key not in a_counter or a_counter[key] - value < 0:
-        ana_chk = False
+    if cntA[ch] < need:
+        print('N')
         break
-    a_counter[key] -= value
-
-if b_counter['*'] != sum(a_counter.values()):
-    ana_chk = False
-
-if ana_chk:
-    print('A')
+    cntA[ch] -= need
 else:
-    print('N')
+    # 길이가 같으므로 남은 문자의 총합은 stars와 같아야 함
+    if sum(cntA.values()) == stars:
+        print('A')
+    else:
+        print('N')
