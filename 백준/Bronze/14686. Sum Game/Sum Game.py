8cbe4877ce073cf762e761352fb8b9b59ba3dcee
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2026-01-21 23:06:06 +0900

    [Bronze II] Title: Sum Game, Time: 72 ms, Memory: 39240 KB -BaekjoonHub

--- 백준/Bronze/14686. Sum Game/Sum Game.py
+++ 백준/Bronze/14686. Sum Game/Sum Game.py
@@ -1,14 +1,31 @@
+# Gemini 3 Pro
 import sys
-input = sys.stdin.readline
 
-N = int(input().strip())
-SW = list(map(int, input().split()))
-SM = list(map(int, input().split()))
-k = 0
-sw_score, sm_score = 0, 0
-for i in range(N):
-    sw_score += SW[i]
-    sm_score += SM[i]
-    if sw_score == sm_score:
-        k = i + 1
-print(k)
+def solve():
+    # 입력 속도 향상
+    input = sys.stdin.readline
+    
+    # 총 일수 N
+    n = int(input())
+    
+    # 두 팀의 점수 리스트
+    swifts = list(map(int, input().split()))
+    semaphores = list(map(int, input().split()))
+    
+    sum_swifts = 0
+    sum_semaphores = 0
+    k = 0
+    
+    # 1일부터 n일까지 점수를 누적하며 비교
+    for i in range(n):
+        sum_swifts += swifts[i]
+        sum_semaphores += semaphores[i]
+        
+        # 누적 점수가 같을 때마다 K를 갱신 (마지막 날을 구해야 하므로)
+        if sum_swifts == sum_semaphores:
+            k = i + 1
+            
+    print(k)
+
+if __name__ == "__main__":
+    solve()
