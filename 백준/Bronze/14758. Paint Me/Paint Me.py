a875e2a63c686ee277bb8098981df798c6c99720
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2026-01-25 23:35:47 +0900

    [Bronze II] Title: Paint Me, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/14758. Paint Me/Paint Me.py
+++ 백준/Bronze/14758. Paint Me/Paint Me.py
@@ -1,14 +1,26 @@
-import sys, math
+# GPT 5.1
+import sys
 input = sys.stdin.readline
 
 while True:
-    N, W, L, H, A, M = map(int, input().split())
-    if N + W + L + H + A + M == 0:
+    n, width, length, height, area, m = map(int, input().split())
+    if n == 0 and width == 0 and length == 0 and height == 0 and area == 0 and m == 0:
         break
-    needed_paint = (L + W) * H * 2 + W * L
-    for _ in range(M):
-        DW, DH = map(int, input().split())
-        needed_paint -= DW * DH
-    total_needed_paint = N * needed_paint
-    r = math.ceil(total_needed_paint / A)
-    print(r)
+
+    # 4개 벽 + 천장 면적
+    wall_area = 2 * (width * height) + 2 * (length * height)
+    ceiling_area = width * length
+    paint_area = wall_area + ceiling_area
+
+    # 문/창문 면적 빼기
+    for _ in range(m):
+        w, h = map(int, input().split())
+        paint_area -= w * h
+
+    # 아파트 개수 반영
+    total_area = paint_area * n
+
+    # 필요한 페인트 캔 수 (올림)
+    cans = (total_area + area - 1) // area
+
+    print(cans)
