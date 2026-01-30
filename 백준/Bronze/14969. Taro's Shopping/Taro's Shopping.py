889b6774baadaa06f6a59dddae0aa3191f25deab
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2026-01-31 00:23:53 +0900

    [Bronze II] Title: Taro's Shopping, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/14969. Taro's Shopping/Taro's Shopping.py
+++ 백준/Bronze/14969. Taro's Shopping/Taro's Shopping.py
@@ -1,15 +1,29 @@
+# GPT 5.1
 import sys
 input = sys.stdin.readline
 
 while True:
-    N, M = map(int, input().split())
-    if N == M == 0:
+    n, m = map(int, input().split())
+    if n == 0 and m == 0:
         break
-    A = list(map(int, input().split()))
-    max_price = 0
-    for i in range(N - 1):
-        for j in range(i + 1, N):
-            price = A[i] + A[j]
-            if price <= M and price > max_price:
-                max_price = price
-    print(max_price if max_price != 0 else 'NONE')
+
+    prices = list(map(int, input().split()))
+    prices.sort()
+
+    left, right = 0, n - 1
+    best = -1
+
+    while left < right:
+        s = prices[left] + prices[right]
+
+        if s <= m:
+            if s > best:
+                best = s
+            left += 1
+        else:
+            right -= 1
+
+    if best == -1:
+        print("NONE")
+    else:
+        print(best)
