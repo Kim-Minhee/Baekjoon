a8cbb9811ac84a9f6f35e82f0e70eab0c595c4c2
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2026-02-07 23:38:05 +0900

    [Bronze II] Title: Arithmetic Sequences, Time: 32 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/15130. Arithmetic Sequences/Arithmetic Sequences.py
+++ 백준/Bronze/15130. Arithmetic Sequences/Arithmetic Sequences.py
@@ -1,26 +1,32 @@
+# GPT 5.1
 import sys
 input = sys.stdin.readline
 
-def solve():
-    N = list(map(int, input().split()))
-    not_zero_num = []
-    not_zero_idx = []
-    for i, n in enumerate(N):
-        if n != 0:
-            not_zero_num.append(n)
-            not_zero_idx.append(i)
-    d = (not_zero_num[1] - not_zero_num[0]) / (not_zero_idx[1] - not_zero_idx[0])
-    if not d.is_integer():
-        print(-1)
-        return
-    else:
-        d = int(d)
-    
-    if not_zero_idx[0] != 0:
-        N[0] = not_zero_num[0] - (not_zero_idx[0] * d)
-    for i in range(1, 10):
-        N[i] = N[i - 1] + d
-    print(*N)
+arr = list(map(int, input().split()))
 
-if __name__ == '__main__':
-    solve()
+# 0이 아닌 값들의 (인덱스, 값)
+known = [(i, arr[i]) for i in range(10) if arr[i] != 0]
+
+i, A = known[0]
+j, B = known[1]
+
+# d가 정수인지 확인
+if (B - A) % (j - i) != 0:
+    print(-1)
+    exit()
+
+d = (B - A) // (j - i)
+
+# 첫 항 계산
+a0 = A - d * i
+
+# 전체 수열 생성
+result = [a0 + d * k for k in range(10)]
+
+# 기존 값들과 일치하는지 검증
+for idx, val in known:
+    if result[idx] != val:
+        print(-1)
+        exit()
+
+print(' '.join(map(str, result)))
