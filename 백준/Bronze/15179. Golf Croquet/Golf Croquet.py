de4900e140257c05b297f23f4c7bc9832c63dddd
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2026-02-10 21:24:29 +0900

    [Bronze II] Title: Golf Croquet, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/15179. Golf Croquet/Golf Croquet.py
+++ 백준/Bronze/15179. Golf Croquet/Golf Croquet.py
@@ -1,48 +1,68 @@
+# GPT 5.1
 import sys
 input = sys.stdin.readline
 
-T1, T2 = input().rstrip(), input().rstrip()
+# 팀 이름 입력
+team1 = input().rstrip()
+team2 = input().rstrip()
+
+# 스트로크 수
 S = int(input().strip())
-RCD = input().strip()
 
-x, y = 0, 0
-win = str()
-for idx, rcd in enumerate(RCD):
-    if rcd == 'S':
-        continue
-    if rcd == 'H':
-        if idx % 2 == 0:
-            x += 1
-        else:
-            y += 1
-    elif rcd == 'D':
-        if idx % 2 == 0:
-            x += 2
-        else:
-            y += 2
+# 스트로크 기록
+shots = input().strip()
+
+# 점수
+score1 = 0
+score2 = 0
+
+# 플레이어 순서: 0=blue, 1=red, 2=black, 3=yellow
+# team1: blue(0), black(2)
+# team2: red(1), yellow(3)
+
+for i in range(S):
+    shot = shots[i]
+    player = i % 4
+
+    # 현재 플레이어 팀
+    if player in (0, 2):
+        my_score, opp_score = score1, score2
+        my_team = 1
     else:
-        if idx % 2 == 0:
-            y += 1
-        else:
-            x += 1
-    
-    if x >= 7:
-        win = T1
-        x = 7
-        break
-    elif y >= 7:
-        win = T2
-        y = 7
+        my_score, opp_score = score2, score1
+        my_team = 2
+
+    if shot == 'H':
+        my_score += 1
+    elif shot == 'D':
+        my_score += 2
+    elif shot == 'O':
+        opp_score += 1
+
+    # 점수 상한 7
+    my_score = min(my_score, 7)
+    opp_score = min(opp_score, 7)
+
+    # 점수 반영
+    if my_team == 1:
+        score1, score2 = my_score, opp_score
+    else:
+        score2, score1 = my_score, opp_score
+
+    # 누군가 이기면 즉시 종료
+    if score1 == 7 or score2 == 7:
         break
 
-print(f'{T1} {x} {T2} {y}.', end = ' ')
-if win == T1:
-    print(f'{T1} has won.')
-elif win == T2:
-    print(f'{T2} has won.')
-elif x > y:
-    print(f'{T1} is winning.')
-elif x < y:
-    print(f'{T2} is winning.')
+# 결과 출력
+print(f"{team1} {score1} {team2} {score2}.", end=" ")
+
+if score1 == 7:
+    print(f"{team1} has won.")
+elif score2 == 7:
+    print(f"{team2} has won.")
+elif score1 > score2:
+    print(f"{team1} is winning.")
+elif score2 > score1:
+    print(f"{team2} is winning.")
 else:
-    print(f'All square.')
+    print("All square.")
