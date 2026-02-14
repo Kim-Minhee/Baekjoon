424f592c101e18296b7b69aae1cfa43be543199c
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2026-02-14 20:09:32 +0900

    [Bronze II] Title: Another Brick in the Wall, Time: 28 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/15216. Another Brick in the Wall/Another Brick in the Wall.py
+++ 백준/Bronze/15216. Another Brick in the Wall/Another Brick in the Wall.py
@@ -1,21 +1,27 @@
+# GPT 5.1
 import sys
 input = sys.stdin.readline
 
-H, W, N = map(int, input().split())
-X = list(map(int, input().split()))
-cur_w, done = 0, False
-for x in X:
-    cur_w += x
-    if cur_w == W:
-        cur_w = 0
-        H -= 1
-        if H == 0:
-            done = True
-            break
-    elif cur_w > W:
-        break
+h, w, n = map(int, input().split())
+bricks = list(map(int, input().split()))
 
-if done:
-    print('YES')
-else:
-    print('NO')
+current_layer = 1
+remain = w
+
+for brick in bricks:
+    if brick <= remain:
+        remain -= brick
+        
+        if remain == 0:
+            current_layer += 1
+            remain = w
+            
+            if current_layer > h:
+                print("YES")
+                exit()
+    else:
+        print("NO")
+        exit()
+
+# 벽돌 다 썼는데 아직 벽 완성 못함
+print("NO")
