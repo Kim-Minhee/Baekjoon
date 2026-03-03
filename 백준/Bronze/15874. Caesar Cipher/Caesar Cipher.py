cc89d6917ce2e63bfe7e2d9f49d0304db69a166a
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2026-03-03 22:43:40 +0900

    [Bronze II] Title: Caesar Cipher, Time: 84 ms, Memory: 33192 KB -BaekjoonHub

--- 백준/Bronze/15874. Caesar Cipher/Caesar Cipher.py
+++ 백준/Bronze/15874. Caesar Cipher/Caesar Cipher.py
@@ -1,17 +1,21 @@
 import sys
 input = sys.stdin.readline
 
-K, L = map(int, input().split())
-S = input().rstrip()
-r = []
-for s in S:
-    if not s.isalpha():
-        r.append(s)
-        continue
-    s_upper = s.upper()
-    k = (ord(s_upper) - ord('A') + K) % 26
-    if s_upper == s:
-        r.append(chr(ord('A') + k))
+k, length = map(int, input().split())
+s = input().rstrip('\n')
+
+k %= 26
+
+result = []
+
+for c in s:
+    if 'A' <= c <= 'Z':
+        new_c = chr((ord(c) - ord('A') + k) % 26 + ord('A'))
+        result.append(new_c)
+    elif 'a' <= c <= 'z':
+        new_c = chr((ord(c) - ord('a') + k) % 26 + ord('a'))
+        result.append(new_c)
     else:
-        r.append(chr(ord('a') + k))
-print(''.join(r))
+        result.append(c)
+
+print(''.join(result))
