55637923b84615ca5b310e546ca114f6741dfc2c
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2026-03-08 21:26:21 +0900

    [Bronze II] Title: Farm, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/16283. Farm/Farm.py
+++ 백준/Bronze/16283. Farm/Farm.py
@@ -1,17 +1,37 @@
-# GPT 5
+# Gemini 3 Pro
 import sys
-input = sys.stdin.readline
 
-a, b, n, w = map(int, input().split())
+def solve():
+    input = sys.stdin.readline
+    
+    # a: 양 1마리 사료, b: 염소 1마리 사료, n: 전체 마릿수, w: 전체 사료 양
+    try:
+        line = input().split()
+        if not line: return
+        a, b, n, w = map(int, line)
+    except ValueError:
+        return
 
-ans = []
+    answers = []
+    
+    # 양(sheep)은 최소 1마리, 최대 n-1마리 가능
+    for sheep in range(1, n):
+        goats = n - sheep # 염소의 수는 전체 - 양의 수
+        
+        # 사료의 총합이 w와 일치하는지 확인
+        if a * sheep + b * goats == w:
+            answers.append((sheep, goats))
+            
+            # 해가 2개 이상이 되는 순간 더 탐색할 필요 없이 중단하여 효율성 극대화
+            if len(answers) > 1:
+                break
+                
+    # 해가 정확히 1개일 때만 결과 출력
+    if len(answers) == 1:
+        print(f"{answers[0][0]} {answers[0][1]}")
+    else:
+        # 해가 없거나(0개) 2개 이상인 경우
+        print("-1")
 
-for x in range(1, n):
-    y = n - x
-    if a * x + b * y == w:
-        ans.append((x, y))
-
-if len(ans) == 1:
-    print(ans[0][0], ans[0][1])
-else:
-    print(-1)
+if __name__ == '__main__':
+    solve()
