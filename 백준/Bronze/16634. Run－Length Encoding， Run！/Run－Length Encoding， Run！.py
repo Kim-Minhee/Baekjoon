c91f64feb8abf0781bbdc22de5d9958366dec041
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2026-03-18 19:28:49 +0900

    [Bronze II] Title: Run-Length Encoding, Run!, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/16634. Run－Length Encoding， Run！/Run－Length Encoding， Run！.py
+++ 백준/Bronze/16634. Run－Length Encoding， Run！/Run－Length Encoding， Run！.py
@@ -1,45 +1,20 @@
-# Gemini 3 Pro
 import sys
+input = sys.stdin.readline
 
-def solve():
-    # 입력받기: 첫 번째 값은 명령어(E/D), 두 번째 값은 메시지
-    input_data = sys.stdin.readline().strip().split()
-    if not input_data:
-        return
-    
-    command = input_data[0]
-    message = input_data[1]
-    
-    if command == 'E':  # 인코딩 (압축하기)
-        encoded_result = []
-        current_char = message[0]
-        count = 1
-        
-        # 두 번째 문자부터 끝까지 확인
-        for i in range(1, len(message)):
-            if message[i] == current_char:
-                count += 1
-            else:
-                # 문자가 달라지면 지금까지 센 문자와 개수를 저장
-                encoded_result.append(current_char + str(count))
-                current_char = message[i]
-                count = 1
-        
-        # 마지막으로 세고 있던 문자와 개수 저장
-        encoded_result.append(current_char + str(count))
-        print("".join(encoded_result))
-        
-    elif command == 'D':  # 디코딩 (압축 풀기)
-        decoded_result = []
-        
-        # 압축된 문자열은 항상 '문자+숫자' 쌍으로 이루어져 있으므로 2칸씩 건너뛰며 반복
-        for i in range(0, len(message), 2):
-            char = message[i]
-            count = int(message[i+1])
-            # 해당 문자를 숫자만큼 반복해서 리스트에 추가
-            decoded_result.append(char * count)
-            
-        print("".join(decoded_result))
-
-if __name__ == '__main__':
-    solve()
+T, S = input().split()
+r = []
+if T == 'E':
+    cur_s, cur_c = S[0], 1
+    for s in S[1:]:
+        if s == cur_s:
+            cur_c += 1
+        else:
+            r.append(cur_s + str(cur_c))
+            cur_s = s
+            cur_c = 1
+    r.append(cur_s + str(cur_c))
+else:
+    for i in range(0, len(S), 2):
+        t, c = S[i], int(S[i + 1])
+        r.append(t * c)
+print(''.join(r))
