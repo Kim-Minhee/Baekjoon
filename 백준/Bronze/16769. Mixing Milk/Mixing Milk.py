1dfe8889518ea579d2eae9bebc4074d0dd768253
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2026-03-20 21:48:34 +0900

    [Bronze II] Title: Mixing Milk, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/16769. Mixing Milk/Mixing Milk.py
+++ 백준/Bronze/16769. Mixing Milk/Mixing Milk.py
@@ -1,21 +1,27 @@
+# GPT 5
 import sys
 input = sys.stdin.readline
 
-milk = []
-buckets = []
+# 용량, 현재 우유
+c = []
+m = []
+
 for _ in range(3):
-    C, M = map(int, input().split())
-    buckets.append(C)
-    milk.append(M)
+    ci, mi = map(int, input().split())
+    c.append(ci)
+    m.append(mi)
 
+# 100번 반복
 for i in range(100):
-    cur, next = i % 3, (i + 1) % 3
-    milk[next] += milk[cur]
-    if milk[next] > buckets[next]:
-        milk[cur] = milk[next] - buckets[next]
-        milk[next] -= milk[cur]
-    else:
-        milk[cur] = 0
+    a = i % 3
+    b = (i + 1) % 3
+    
+    # 옮길 수 있는 양
+    pour = min(m[a], c[b] - m[b])
+    
+    m[a] -= pour
+    m[b] += pour
 
-for m in milk:
-    print(m)
+# 출력
+for x in m:
+    print(x)
