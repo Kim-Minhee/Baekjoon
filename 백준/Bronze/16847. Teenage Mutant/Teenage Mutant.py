a6c736b5ee93a5a4714c5cad3c7235d4c5f52710
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2026-03-22 20:24:50 +0900

    [Bronze II] Title: Teenage Mutant, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/16847. Teenage Mutant/Teenage Mutant.py
+++ 백준/Bronze/16847. Teenage Mutant/Teenage Mutant.py
@@ -1,20 +1,26 @@
+# GPT 5
 import sys
 input = sys.stdin.readline
 
-for x in range(1, int(input().strip()) + 1):
-    N, K = map(int, input().split())
-    MY = input().strip()
-    pg = {}
-    for _ in range(N):
-        G = input().strip()
-        for i in range(K):
-            temp = pg.get(i, '')
-            pg[i] = temp + G[i]
+K = int(input())
 
-    cnt = 0
-    for i in range(K):
-        if MY[i] not in pg[i]:
-            cnt += 1
-    print(f'Data Set {x}:')
-    print(f'{cnt}/{K}')
+for tc in range(1, K + 1):
+    n, k = map(int, input().split())
+    
+    me = input().strip()
+    ancestors = [input().strip() for _ in range(n)]
+    
+    mutant = 0
+    
+    for i in range(k):
+        is_mutant = True
+        for j in range(n):
+            if ancestors[j][i] == me[i]:
+                is_mutant = False
+                break
+        if is_mutant:
+            mutant += 1
+    
+    print(f"Data Set {tc}:")
+    print(f"{mutant}/{k}")
     print()
