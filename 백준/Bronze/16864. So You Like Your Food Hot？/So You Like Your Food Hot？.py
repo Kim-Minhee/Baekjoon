c73b5ef05481da2056e6b69f3f688f9f0d984e92
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2026-03-26 22:41:27 +0900

    [Bronze II] Title: So You Like Your Food Hot?, Time: 248 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/16864. So You Like Your Food Hot？/So You Like Your Food Hot？.py
+++ 백준/Bronze/16864. So You Like Your Food Hot？/So You Like Your Food Hot？.py
@@ -1,17 +1,28 @@
+# GPT 5
 import sys
 input = sys.stdin.readline
 
-PT, P1, P2 = map(float, input().split())
-PT = int(round(PT * 100))
-P1 = int(round(P1 * 100))
-P2 = int(round(P2 * 100))
+pt, p1, p2 = map(float, input().split())
 
-chk = False
-for i in range(int(PT / P1) + 1):
-    p = PT - (P1 * i)
-    if p % P2 == 0:
-        print(i, int(p // P2))
-        chk = True
+# 센트 단위로 변환
+pt = int(round(pt * 100))
+p1 = int(round(p1 * 100))
+p2 = int(round(p2 * 100))
 
-if not chk:
-    print('none')
+found = False
+
+# 피타 개수 x 기준 (작은 순서 출력)
+for x in range(pt // p1 + 1):
+    remain = pt - x * p1
+    
+    if remain < 0:
+        break
+    
+    # 피자 개수 y 계산 가능 여부
+    if remain % p2 == 0:
+        y = remain // p2
+        print(x, y)
+        found = True
+
+if not found:
+    print("none")
