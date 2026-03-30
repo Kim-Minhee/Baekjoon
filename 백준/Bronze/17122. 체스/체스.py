b7aac175f76cd4623a37c4cc623a243eaaad80eb
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2026-03-30 23:29:57 +0900

    [Bronze II] Title: 체스, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/17122. 체스/체스.py
+++ 백준/Bronze/17122. 체스/체스.py
@@ -1,23 +1,34 @@
-# GPT 5
+# Gemini 3.1 Pro
 import sys
-input = sys.stdin.readline
 
-T = int(input())
+def solve():
+    input = sys.stdin.readline
+    
+    # 첫 줄: 테스트 케이스의 수 T
+    try:
+        T = int(input().strip())
+    except ValueError:
+        return
+        
+    for _ in range(T):
+        pos1, pos2 = input().split()
+        
+        # 표기법 1 (예: "A1") 분해
+        # 'A'의 아스키코드는 65이므로, ord('A')를 빼고 1을 더하면 1~8의 숫자로 변환됩니다.
+        col1 = ord(pos1[0]) - ord('A') + 1
+        row1 = int(pos1[1])
+        
+        # 표기법 2 (예: "17") 분해
+        num = int(pos2)
+        # 1부터 시작하는 인덱스를 0부터 시작하도록 1을 뺀 뒤 계산하고 다시 1을 더해줍니다.
+        col2 = (num - 1) % 8 + 1
+        row2 = (num - 1) // 8 + 1
+        
+        # 두 좌표의 합이 홀수인지 짝수인지(나머지 2) 비교합니다.
+        if (col1 + row1) % 2 == (col2 + row2) % 2:
+            print("YES")
+        else:
+            print("NO")
 
-for _ in range(T):
-    s, num = input().split()
-    num = int(num)
-    
-    # 표기법1
-    col1 = ord(s[0]) - ord('A') + 1
-    row1 = int(s[1])
-    
-    # 표기법2
-    row2 = (num - 1) // 8 + 1
-    col2 = (num - 1) % 8 + 1
-    
-    # 색 비교
-    if (row1 + col1) % 2 == (row2 + col2) % 2:
-        print("YES")
-    else:
-        print("NO")
+if __name__ == '__main__':
+    solve()
