5a6b6e5d92aaf4741a3694ed82999138bedaf95e
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2026-03-30 23:17:42 +0900

    [Bronze II] Title: 체스, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/17122. 체스/체스.py
+++ 백준/Bronze/17122. 체스/체스.py
@@ -1,18 +1,23 @@
+# GPT 5
 import sys
 input = sys.stdin.readline
 
-black = ['A1', 'A3', 'A5', 'A7', 'B2', 'B4', 'B6', 'B8',
-         'C1', 'C3', 'C5', 'C7', 'D2', 'D4', 'D6', 'D8',
-         'E1', 'E3', 'E5', 'E7', 'F2', 'F4', 'F6', 'F8',
-         'G1', 'G3', 'G5', 'G7', 'H2', 'H4', 'H6', 'H8',
-         '1', '3', '5', '7', '10', '12', '14', '16',
-         '17', '19', '21', '23', '26', '28', '30', '32',
-         '33', '35', '37', '39', '42', '44', '46', '48',
-         '49', '51', '53', '55', '58', '60', '62', '64']
+T = int(input())
 
-for _ in range(int(input().strip())):
-    M1, M2 = input().split()
-    if (M1 in black and M2 in black) or (M1 not in black and M2 not in black):
-        print('YES')
+for _ in range(T):
+    s, num = input().split()
+    num = int(num)
+    
+    # 표기법1
+    col1 = ord(s[0]) - ord('A') + 1
+    row1 = int(s[1])
+    
+    # 표기법2
+    row2 = (num - 1) // 8 + 1
+    col2 = (num - 1) % 8 + 1
+    
+    # 색 비교
+    if (row1 + col1) % 2 == (row2 + col2) % 2:
+        print("YES")
     else:
-        print('NO')
+        print("NO")
