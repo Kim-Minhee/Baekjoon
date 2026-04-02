d6f42fdae015edd309fa4daf5c420400261657b2
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2026-04-02 22:53:53 +0900

    [Bronze II] Title: 맥주 99병, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/17293. 맥주 99병/맥주 99병.py
+++ 백준/Bronze/17293. 맥주 99병/맥주 99병.py
@@ -1,23 +1,39 @@
+# Gemini 3.1 Pro
 import sys
-input = sys.stdin.readline
 
-N = int(input().strip())
-k = N
-while k > 1:
-    print(f'{k} bottles of beer on the wall, {k} bottles of beer.')
-    k -= 1
-    if k != 1:
-        print(f'Take one down and pass it around, {k} bottles of beer on the wall.')
+# 병의 개수에 따른 올바른 문자열을 반환하는 함수
+def get_bottle_str(count):
+    if count > 1:
+        return f"{count} bottles"
+    elif count == 1:
+        return "1 bottle"
     else:
-        print(f'Take one down and pass it around, 1 bottle of beer on the wall.')
-    print()
+        return "no more bottles"
 
-print('1 bottle of beer on the wall, 1 bottle of beer.')
-print('Take one down and pass it around, no more bottles of beer on the wall.')
-print()
+def solve():
+    # 입력값 N 받기
+    input_data = sys.stdin.read().split()
+    if not input_data:
+        return
+        
+    n = int(input_data[0])
+    
+    # N부터 1까지 역순으로 반복
+    for k in range(n, 0, -1):
+        curr_str = get_bottle_str(k)
+        next_str = get_bottle_str(k - 1)
+        
+        # 현재 절 출력
+        print(f"{curr_str} of beer on the wall, {curr_str} of beer.")
+        print(f"Take one down and pass it around, {next_str} of beer on the wall.")
+        
+        # 각 절 사이에는 빈 줄 출력
+        print()
+        
+    # 반복문이 모두 끝나고 마지막 가사 출력
+    # (문장 시작이므로 대문자 'No' 사용)
+    print("No more bottles of beer on the wall, no more bottles of beer.")
+    print(f"Go to the store and buy some more, {get_bottle_str(n)} of beer on the wall.")
 
-print('No more bottles of beer on the wall, no more bottles of beer.')
-if N > 1:
-    print(f'Go to the store and buy some more, {N} bottles of beer on the wall.')
-else:
-    print(f'Go to the store and buy some more, 1 bottle of beer on the wall.')
+if __name__ == '__main__':
+    solve()
