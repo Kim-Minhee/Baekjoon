7a40ff7c213b3cb12a7d804386979cd8bf8ce02a
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2026-04-08 21:50:37 +0900

    [Bronze II] Title: This Problem’s a Slam Dunk, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/17851. This Problem’s a Slam Dunk/This Problem’s a Slam Dunk.py
+++ 백준/Bronze/17851. This Problem’s a Slam Dunk/This Problem’s a Slam Dunk.py
@@ -6,8 +6,5 @@ UJ = list(map(int, input().split()))
 
 JU.sort(reverse=True)
 UJ.sort(reverse=True)
-cnt = 0
-for i in range(5):
-    if JU[i] > UJ[i]:
-        cnt += 1
+cnt = sum(1 for i in range(5) if JU[i] > UJ[i])
 print(cnt)
