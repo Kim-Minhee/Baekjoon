a3a098fc5bb1c1d8e326de0b725ce0503ac82663
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2026-04-09 17:26:57 +0900

    [Bronze II] Title: Mars Window, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/17889. Mars Window/Mars Window.py
+++ 백준/Bronze/17889. Mars Window/Mars Window.py
@@ -1,12 +1,22 @@
+# GPT 5
 import sys
 input = sys.stdin.readline
 
-years = []
-for i in range(2018, 10001, 13):
-    years += [i, i + 2, i + 4, i + 6, i + 8, i + 11]
+y = int(input().strip())
 
-Y = int(input().strip())
-if Y in years:
-    print('yes')
+# 시작 시점 (월 단위)
+start = 2018 * 12 + 4
+
+# 해당 연도의 시작/끝
+year_start = y * 12 + 1
+year_end = y * 12 + 12
+
+cur = start
+
+while cur <= year_end:
+    if year_start <= cur <= year_end:
+        print("yes")
+        break
+    cur += 26
 else:
-    print('no')
+    print("no")
