fc55595d16e23e7cdc7104a4098ef1dba8cfb08e
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2026-04-16 14:52:24 +0900

    [Bronze II] Title: Howl, Time: 128 ms, Memory: 43596 KB -BaekjoonHub

--- 백준/Bronze/17987. Howl/Howl.py
+++ 백준/Bronze/17987. Howl/Howl.py
@@ -1,5 +1,16 @@
+# GPT 5
 import sys
 input = sys.stdin.readline
 
-F = input().strip()
-print(F + 'O')
+fenrir = input().strip()
+n = len(fenrir)
+
+# 기본 구조
+res = list("AHOW")
+
+# 길이를 Fenrir보다 길게 만들기
+while len(res) <= n:
+    res.append('O')
+    res.append('W')
+
+print(''.join(res))
