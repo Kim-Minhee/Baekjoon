3159d70efa6288f84f09cd127f342bfbdbdc3050
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2026-04-17 13:53:06 +0900

    [Bronze II] Title: Совпадения, Time: 60 ms, Memory: 47788 KB -BaekjoonHub

--- 백준/Bronze/18098. Совпадения/Совпадения.py
+++ 백준/Bronze/18098. Совпадения/Совпадения.py
@@ -1,24 +1,26 @@
-# GPT 5
+# Gemini 3.1 Pro
 import sys
-input = sys.stdin.readline
 
-N = int(input())
-A = [int(input()) for _ in range(N)]
+def solve():
+    # 입력 속도를 높이기 위해 sys.stdin.read 사용
+    input_data = sys.stdin.read().split()
+    if not input_data:
+        return
+    
+    n = int(input_data[0])
+    passports = input_data[1:]
+    
+    # 중복을 제거하면서 조건에 맞는 번호만 담을 Set 생성
+    matched_rooms = set()
+    
+    for p in passports:
+        num = int(p)
+        # 여권 번호가 1 이상 N 이하인 경우에만 일치 가능
+        if 1 <= num <= n:
+            matched_rooms.add(num)
+            
+    # 고유한 일치 가능 방의 개수 출력
+    print(len(matched_rooms))
 
-A.sort()
-
-room = 1
-i = 0
-ans = 0
-
-while room <= N and i < N:
-    if A[i] == room:
-        ans += 1
-        room += 1
-        i += 1
-    elif A[i] < room:
-        i += 1
-    else:
-        room += 1
-
-print(ans)
+if __name__ == "__main__":
+    solve()
