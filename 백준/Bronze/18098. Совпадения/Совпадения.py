960c57fc6285da0c369ab4a4ee104a3db0d1adfe
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2026-04-17 13:52:32 +0900

    [Bronze II] Title: Совпадения, Time: 116 ms, Memory: 36264 KB -BaekjoonHub

--- 백준/Bronze/18098. Совпадения/Совпадения.py
+++ 백준/Bronze/18098. Совпадения/Совпадения.py
@@ -1,10 +1,24 @@
+# GPT 5
 import sys
 input = sys.stdin.readline
 
-N = int(input().strip())
-rooms = [0] * (N + 1)
-for _ in range(N):
-    A = int(input().strip())
-    if A <= N:
-        rooms[A] = 1
-print(sum(rooms))
+N = int(input())
+A = [int(input()) for _ in range(N)]
+
+A.sort()
+
+room = 1
+i = 0
+ans = 0
+
+while room <= N and i < N:
+    if A[i] == room:
+        ans += 1
+        room += 1
+        i += 1
+    elif A[i] < room:
+        i += 1
+    else:
+        room += 1
+
+print(ans)
