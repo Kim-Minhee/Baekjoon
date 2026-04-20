51ec61e3b50805f6b3179119f08272c92b5a91bb
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2026-04-20 14:26:46 +0900

    [Bronze II] Title: Speeding, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/18154. Speeding/Speeding.py
+++ 백준/Bronze/18154. Speeding/Speeding.py
@@ -1,13 +1,19 @@
+# GPT 5
 import sys
 input = sys.stdin.readline
 
-cur_t, cur_d, max_s = 0, 0, 0
-for i in range(int(input().strip())):
-    T, D = map(int, input().split())
-    if i == 0:
-        continue
-    t, d = T - cur_t, D - cur_d
-    s = int(d / t)
-    max_s = max(max_s, s)
-    cur_t, cur_d = T, D
-print(max_s)
+N = int(input())
+
+t_prev, d_prev = map(int, input().split())
+
+max_speed = 0
+
+for _ in range(N - 1):
+    t, d = map(int, input().split())
+    
+    speed = (d - d_prev) // (t - t_prev)
+    max_speed = max(max_speed, speed)
+    
+    t_prev, d_prev = t, d
+
+print(max_speed)
