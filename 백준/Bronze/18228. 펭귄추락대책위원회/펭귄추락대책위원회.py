c8c4a5edae77d05b1786a393e557b82ba3151b82
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2026-04-21 09:54:35 +0900

    [Bronze II] Title: 펭귄추락대책위원회, Time: 96 ms, Memory: 56452 KB -BaekjoonHub

--- 백준/Bronze/18228. 펭귄추락대책위원회/펭귄추락대책위원회.py
+++ 백준/Bronze/18228. 펭귄추락대책위원회/펭귄추락대책위원회.py
@@ -1,21 +1,17 @@
+# GPT 5
 import sys
 input = sys.stdin.readline
 
-N = int(input().strip())
+N = int(input())
 A = list(map(int, input().split()))
-left, right = 0, N - 1
-index_p = A.index(-1)
-if 1 in A:
-    index_1 = A.index(1)
-    if index_1 < index_p:
-        left = index_1
-    else:
-        right = index_1
-if N in A:
-    index_n = A.index(N)
-    if index_n < index_p:
-        left = index_n
-    else:
-        right = index_n
-r = min(A[left:index_p]) + min(A[index_p + 1:right + 1])
-print(r)
+
+# 펭귄 위치 찾기
+K = A.index(-1)
+
+# 왼쪽 최소
+left_min = min(A[:K])
+
+# 오른쪽 최소
+right_min = min(A[K+1:])
+
+print(left_min + right_min)
