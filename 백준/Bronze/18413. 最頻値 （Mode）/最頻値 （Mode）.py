038a9a06c6b0e08b01bf86e717749ef21e0ab942
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2026-04-24 08:50:37 +0900

    [Bronze II] Title: 最頻値 (Mode), Time: 52 ms, Memory: 34900 KB -BaekjoonHub

--- 백준/Bronze/18413. 最頻値 （Mode）/最頻値 （Mode）.py
+++ 백준/Bronze/18413. 最頻値 （Mode）/最頻値 （Mode）.py
@@ -1,13 +1,8 @@
-# GPT 5
+from collections import Counter
 import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
 A = list(map(int, input().split()))
-
-count = [0] * (M + 1)
-
-for x in A:
-    count[x] += 1
-
-print(max(count))
+counter = Counter(A)
+print(max(counter.values()))
