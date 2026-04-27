c25db02d932a32aa80fcec57c8a8e9c1fe19c25d
Kim-Minhee <71242751+Kim-Minhee@users.noreply.github.com>
2026-04-28 08:52:56 +0900

    [Bronze II] Title: 이동하기 3, Time: 272 ms, Memory: 113820 KB -BaekjoonHub

--- 백준/Bronze/18795. 이동하기 3/이동하기 3.py
+++ 백준/Bronze/18795. 이동하기 3/이동하기 3.py
@@ -1,21 +1,7 @@
-# Gemini 3.1 Pro
 import sys
+input = sys.stdin.readline
 
-def solve():
-    # 입력 속도를 높이기 위해 한 번에 통째로 읽어옵니다.
-    input_data = sys.stdin.read().split()
-    if not input_data:
-        return
-    
-    # N과 M 값은 사실 더할 필요가 없으므로 건너뜁니다.
-    # input_data[0]은 N, input_data[1]은 M
-    
-    # 배열 A와 B의 원소들은 input_data의 2번 인덱스부터 끝까지 들어있습니다.
-    # 어떤 경로로 가든 모든 A 원소와 모든 B 원소를 한 번씩 더하게 되므로,
-    # 남아있는 모든 값들의 총합을 구하면 됩니다.
-    total_garbage = sum(int(x) for x in input_data[2:])
-    
-    print(total_garbage)
-
-if __name__ == "__main__":
-    solve()
+N, M = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+print(sum(A) + sum(B))
